795f5f9ab68a7a2c4bcc0496b5776c5220bab5b1
Janeillman <janeillman2@gmail.com>
2023-09-12 17:14:46 -0400

    playing with colour

--- Experiments/Alien /js/script.js
+++ Experiments/Alien /js/script.js
@@ -23,16 +23,22 @@ function setup() {
 
     createCanvas(500, 500);
 
-background(255, 138, 200);
+background(130, 0, 150);
+
 
 point(250, 250);
 
 rect(100, 100, 300, 300);
-
+fill(50,134,67)
+rect(200, 200, 200, 200)
 line(0, 0, 500, 500); 
 line(500, 0, 0, 500);
 
-ellipse(250,250,50,50);
+fill(200, 100, 150);
+ellipse(250,250,50,50); 
+
+
+
 
 }
 

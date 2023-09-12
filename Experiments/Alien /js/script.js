2ad28246217fea82385837f0cd937b0d92bbbfc9
Janeillman <janeillman2@gmail.com>
2023-09-12 15:11:21 -0400

    pink square playing with shapes

--- Experiments/Alien /js/script.js
+++ Experiments/Alien /js/script.js
@@ -21,6 +21,19 @@ function preload() {
 */
 function setup() {
 
+    createCanvas(500, 500);
+
+background(255, 138, 200);
+
+point(250, 250);
+
+rect(100, 100, 300, 300);
+
+line(0, 0, 500, 500); 
+line(500, 0, 0, 500);
+
+ellipse(250,250,50,50);
+
 }
 
 

1d7a4203fb15b0eb593b353a6b57c512dbef138f
Janeillman <janeillman2@gmail.com>
2023-09-12 17:30:22 -0400

    tried to make a face

--- Experiments/Alien /js/script.js
+++ Experiments/Alien /js/script.js
@@ -23,22 +23,17 @@ function setup() {
 
     createCanvas(500, 500);
 
-background(130, 0, 150);
+background(191, 255, 199);
 
+fill(250, 200, 200);
+ellipse(250, 250, 200, 200);
 
-point(250, 250);
+fill(0);
+ellipse(200, 250, 30, 30);
+ellips(300, 250, 30, 30);
 
-rect(100, 100, 300, 300);
-fill(50,134,67)
-rect(200, 200, 200, 200)
-line(0, 0, 500, 500); 
-line(500, 0, 0, 500);
-
-fill(200, 100, 150);
-ellipse(250,250,50,50); 
-
-
-
+strokeWeight(10);
+line(200, 300, 300, 300);
 
 }
 

533fcad9fa2e652ec6e89926cc26198acda2e73a
Janeillman <janeillman2@gmail.com>
2023-09-14 14:37:26 -0400

    commenting code

--- Experiments/Alien /js/script.js
+++ Experiments/Alien /js/script.js
@@ -21,16 +21,21 @@ function preload() {
 */
 function setup() {
 
-createCanvas(500, 500);
-background(255, 0, 0);
-
-fill(134, 200, 190);
-rect(100, 300, 300, 50);
-
-fill(159, 230, 50);
-ellipse(150, 200, 70, 70);
-ellipse(350, 200, 70, 70);
-
+    createCanvas(800, 800);
+    background(51, 20);
+// HEAD
+    ellipseMode(CENTER);
+    fill(0, 230, 50);
+    ellipse(400, 400, 400, 400);
+// MOUTH
+    rectMode(CENTER);
+    fill(134, 200, 190);
+    rect(400, 450, 300, 50);
+// EYES
+    ellipseMode(CORNER);
+    fill(159, 230, 50);
+    ellipse(300, 300, 70, 70);
+    ellipse(500, 300, 70, 70);
 }
 
 

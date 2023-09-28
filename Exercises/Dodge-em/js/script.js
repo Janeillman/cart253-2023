a3fb63857418337e5cb000abd271d3d005a0a023
Janeillman <janeillman2@gmail.com>
2023-09-28 15:58:03 -0400

    E02: inserted mouse and cat images

--- Exercises/Dodge-em/js/script.js
+++ Exercises/Dodge-em/js/script.js
@@ -11,15 +11,38 @@
 /**
  * Description of preload
 */
+
+
+
+
 function preload() {
 
+    mouseFace = loadImage("assets/images/catFace.jpeg");
+    catFace = loadImage("assets/images/mouseFace.jpeg");
+
 }
+
+let catFace = {
+    x: 100,
+    y: 0,
+    size: 100,
+    speed: 1,
+};
+
+let mouseFace = {
+    x: 400,
+    y: 0,
+    size: 75,
+    speed: 1,
+};
 
 
 /**
  * Description of setup
 */
 function setup() {
+
+    createCanvas(windowWidth, windowHeight);
 
 }
 
@@ -29,4 +52,12 @@ function setup() {
 */
 function draw() {
 
+background(0);
+
+imageMode(CENTER);
+image(mouseFace, 200, 200);
+image(catFace, mouseX, mouseY, 100, 100);
+
+
 }
+

988092685382cfa24b8e59b049f26aae6ce9ddeb
Janeillman <janeillman2@gmail.com>
2023-10-04 13:41:23 -0400

    Activity5: working on it
    made circles appear and move randomly; organized code

--- Activities/looking-for-love/js/script.js
+++ Activities/looking-for-love/js/script.js
@@ -8,25 +8,89 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
+
+
+
+
+
 function preload() {
 
 }
 
 
-/**
- * Description of setup
-*/
+
+let circle1 = {
+    x: 150,
+    y: 250, 
+    size:100,
+    vx: 0,
+    vy: 0,
+    speed: 3
+
+};
+
+let circle2 = {
+    x: 350,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0, 
+    speed: 3
+}; 
+
+
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
+    setupCircles();
 }
 
+function setupCircles() {
+    circle1.x = circle1.x;
+    circle2.x = circle2.x;
 
-/**
- * Description of draw()
-*/
+    circle1.vx = random(-circle1.speed, circle1.speed);
+    circle1.vy = random(-circle1.speed, circle1.speed);
+    circle2.vx = random(-circle2.speed, circle2.speed);
+    circle2.vy = random(-circle2.speed, circle2.speed);
+}
+
 function draw() {
+    background(0);
+ 
+        move();
+        checkOffScreen();
+        checkOverlap();
+        display();
 
 }
+
+function move() {
+    // move the circles
+    circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+}
+
+function checkOffScreen() {
+    // check if circles go off screen
+    if (circle1.x < 0|| circle1.x > width ||circle1.y < 0 || circle1.y > height || circle2.x < 0 ||circle2.x > width|| circle2.y < 0 || circle2.y > height) {
+        // sad ending
+    }
+}
+
+function checkOverlap() {
+    // check if circles touch
+    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+    if (d < circle1.size/2 + circle2.size/2) {
+        // love ending
+    }
+}
+
+function display() {
+        // display circles
+        ellipse(circle1.x, circle1.y, circle1.size);
+        ellipse(circle2.x, circle2.y, circle2.size);
+}
+

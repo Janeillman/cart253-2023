1ff79b61c8ecbffb47c5e841504ad8939ec01e44
Janeillman <janeillman2@gmail.com>
2023-10-15 00:15:45 -0400

    P1: added one asteroid

--- Projects/Project-one/js/script.js
+++ Projects/Project-one/js/script.js
@@ -29,12 +29,29 @@ let astronaut = {
     size: 100
 }
 
+let asteroid = {
+    x: 250,
+    y: 250,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    size: 100,
+    fill: {
+        r: 118,
+        g: 130,
+        b: 133
+    }
+}
+
 /**
 Description of setup
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
     stroke(255);
+
+    asteroid.y = random(0, height);
+    asteroid.vx = asteroid.speed;
 }
 
 /**
@@ -46,13 +63,30 @@ function draw() {
     useArrowKeys();
     displayStars();
     displayAstronaut();
+    displayAsteroids();
 
 function displayStars() {
     for (let i = 0; i < 100; i++) {
     let x = random(0, width);
     let y = random(0, height);
-    point(x, y);
+    ellipse(x, y, 1.5, 1.5);
     }
+}
+
+function displayAsteroids() {
+
+    for (let i = 0; i< 5; i++ )
+    ellipse(asteroid.x, asteroid.y, asteroid.size);
+    fill(asteroid.fill.r, asteroid.fill.g, asteroid.fill.b);
+
+    asteroid.x = asteroid.x + asteroid.vx;
+    asteroid.y = asteroid.y + asteroid.vy;
+    
+    if (asteroid.x > width) {
+        asteroid.x = 0;
+        asteroid.y = random(0, height);
+    }
+
 }
 
 function useArrowKeys() {

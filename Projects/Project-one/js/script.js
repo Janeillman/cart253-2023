7ede54b1ae874a8fa89e7aad3ad2acaf8105cbfc
Janeillman <janeillman2@gmail.com>
2023-10-14 20:46:08 -0400

    P1: starry background and astronaut

--- Projects/Project-one/js/script.js
+++ Projects/Project-one/js/script.js
@@ -12,22 +12,38 @@ author, and this description to match your project!
 /**
 Description of preload
 */
+
+let astronaut;
+
 function preload() {
-
+    astronaut = loadImage("assets/images/astronaut.webp");
 }
-
 
 /**
 Description of setup
 */
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    stroke(255);
 
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+
+background(0);
+
+
+for (let i = 0; i < 100; i++) {
+
+  let x = random(0, width);
+  let y = random(0, height);
+
+  point(x, y);
+}
+
+image(astronaut, width/2, height/2);
 
 }

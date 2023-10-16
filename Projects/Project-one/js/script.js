4b647bc621688a640efa07f5c729413be06b4264
Janeillman <janeillman2@gmail.com>
2023-10-16 18:38:36 -0400

    P1: loops and states

--- Projects/Project-one/js/script.js
+++ Projects/Project-one/js/script.js
@@ -39,9 +39,16 @@ let asteroid = {
     fill: {
         r: 118,
         g: 130,
-        b: 133
-    }
+        b: 133,
+    },
+    spacing: 400
 }
+
+let titleString = "Space";
+let endingString = "Game Over";
+let state = `title`; 
+// possible states are `title`, `animation`, `ending`
+
 
 /**
 Description of setup
@@ -49,6 +56,10 @@ Description of setup
 function setup() {
     createCanvas(windowWidth, windowHeight);
     stroke(255);
+
+  // Text settings
+  textSize(32);
+  textAlign(CENTER, CENTER);
 
     asteroid.y = random(0, height);
     asteroid.vx = asteroid.speed;
@@ -60,10 +71,44 @@ Description of draw()
 function draw() {
 
     background(0);
-    useArrowKeys();
     displayStars();
     displayAstronaut();
+    checkOverlap();
+    
+
+  if (state === `title`) {
+      fill(255);
+      text(titleString, width / 2, height / 2);
+    }
+  else if (state === `animation`) { 
+    useArrowKeys();
     displayAsteroids();
+      // NEW!
+      // And we change to the ending state if the circle reaches the right side
+      if (circle.x > width) {
+        state = `ending`;
+      }
+    }
+    else if (state === `ending`) {
+      fill(255, 0, 0);
+      text(endingString, width / 2, height / 2)
+    }
+}
+
+function checkOverlap() {
+    let d = dist(astronaut.x, astronaut.y, asteroid.x, asteroid.y);
+      if(d < asteroid.size/2 + astronaut.size/2) {
+      state = `ending`;
+  }
+}
+
+function keyPressed() {
+    // If any key is pressed, we check if the current state is the title state
+    if (state === `title`) {
+      // If it is, we switch to the animation state
+      state = `animation`;
+    }
+  }
 
 function displayStars() {
     for (let i = 0; i < 100; i++) {
@@ -75,10 +120,14 @@ function displayStars() {
 
 function displayAsteroids() {
 
-    for (let i = 0; i< 5; i++ )
-    ellipse(asteroid.x, asteroid.y, asteroid.size);
+  let x = asteroid.x;
+  let y = asteroid.y;
+    for (let i = 0; i< 5; i++ ) {
+    ellipse(x, y, asteroid.size);
+    x = x + asteroid.spacing;
+    y = y + asteroid.spacing;
     fill(asteroid.fill.r, asteroid.fill.g, asteroid.fill.b);
-
+    }
     asteroid.x = asteroid.x + asteroid.vx;
     asteroid.y = asteroid.y + asteroid.vy;
     
@@ -120,4 +169,3 @@ function displayAstronaut() {
 
 
 
-}

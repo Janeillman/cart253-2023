d3e0baa1664a7d30bd9b1513af30e34c3c18fbf5
Janeillman <janeillman2@gmail.com>
2023-11-16 13:56:23 -0500

    how to make sound, dog barks

--- Experiments/sound experiments/js/script.js
+++ Experiments/sound experiments/js/script.js
@@ -1,32 +1,24 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
 
 "use strict";
 
-/**
- * Description of preload
-*/
+let barkSFX;
+
 function preload() {
-
+    barkSFX = loadSound(`assets/sounds/bark.wav`);
 }
 
 
-/**
- * Description of setup
-*/
+
 function setup() {
-
+    createCanvas(600, 600);
 }
 
 
-/**
- * Description of draw()
-*/
-function draw() {
 
+function draw() {
+    background(0);
+}
+
+function mousePressed() {
+    barkSFX.play();
 }

b239a8ba6db9d59c000690f34aa61d98b345994c
Janeillman <janeillman2@gmail.com>
2023-11-21 19:27:12 -0500

    class Planet: display method

--- Projects/Project-two/js/Planet.js
+++ Projects/Project-two/js/Planet.js
@@ -1,13 +1,22 @@
 class Planet {
 
     constructor(x,y) {
-        this.x = 100
-        this.y = 200
-        this.size = 50
+        this.x = random(0, 800);
+        this.y = random(0, 600);
+        this.size = random(20, 200);
+        this.fill = {
+            r: random(200,255),
+            g: random(200,255),
+            b: random(200,255)
+          };
     }
 
     display() {
-        
+        push();
+        noStroke();
+        fill(this.fill.r,this.fill.g,this.fill.b);
+        ellipse(this.x,this.y,this.size);
+        pop();
     }
 
 }

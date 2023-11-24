829d109c56f57eeb52161c5e44f1adfc47f6b3d6
Janeillman <janeillman2@gmail.com>
2023-11-24 17:40:11 -0500

    doing things

---
+++ Projects/Project2experiment/js/Asteroid.js
@@ -0,0 +1,40 @@
+class Asteroid {
+    
+    constructor(x,y) {
+        this.x = 100;
+        this.y = 100;
+        this.vx = 0;
+        this.vy = 0;
+        this.speed = 12;
+        this.size = 100;
+        this.fill = {
+            r: 211,
+            g: 212,
+            b: 219,
+        };
+        this.spacing = {
+          x: 500,
+          y: 500
+        };
+    }
+
+    move() {
+        this.vx = this.speed;
+      
+          this.x = this.x + this.vx;
+          this.y = this.y + this.vy;
+          
+        if (this.x > width) {
+              this.x = 0;
+              this.y = random(0, height);
+          }
+    }
+
+    display() {
+        fill(this.fill.r, this.fill.g, this.fill.b);
+        ellipse(this.x, this.y, this.size);
+        // this.x = this.x + this.spacing.x;
+        // this.y = this.y + this.spacing.y;
+    }
+
+}
--- Projects/Project2experiment/js/script.js
+++ Projects/Project2experiment/js/script.js
@@ -1,32 +1,48 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let astronautImage;
 
+function preload() {
+    astronautImage = loadImage("assets/images/astronaut.webp");
 }
 
+let astronaut = {
+    astronautArray: [],
+}
 
-/**
- * Description of setup
-*/
+let asteroids = {
+    asteroidArray: [],
+    numAsteroids: 3,
+};
+
+
 function setup() {
 
+    createCanvas(windowWidth, windowHeight);
+
+    for (let i = 0; i < asteroids.numAsteroids; i++) {
+        let asteroid = new Asteroid();
+        asteroids.asteroidArray.push(asteroid);
+      }
+
+      for (let i = 0; i < astronaut.length; i++) {
+        let astronaut = new Astronaut();
+        astronaut.astronautArray.push(astronaut);
+      }
+     
 }
 
 
-/**
- * Description of draw()
-*/
+
 function draw() {
 
+    background(0);
+
+    for (let i = 0; i < asteroids.asteroidArray.length; i++) {
+        let asteroid = asteroids.asteroidArray[i];
+        asteroid.display(); 
+        asteroid.move();
+      }
 }

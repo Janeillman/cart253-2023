c8eca54cb092aa71fcc57c8605f88e99ab24acf5
Janeillman <janeillman2@gmail.com>
2023-12-05 19:07:44 -0500

    adding comments and style

--- Projects/final-project/js/script.js
+++ Projects/final-project/js/script.js
@@ -1,4 +1,9 @@
+/***********************
+Space Game
+by Jane Illman
+Description: 
 
+***********************/
 
 "use strict";
 
@@ -17,12 +22,12 @@ let astronaut = {
   vy: 0,
   speed: 7,
   size: 60
-}
+};
 
 let stars = {
   starArray: [],
   numStars: 100,
-}
+};
 
 let asteroids = {
   asteroidArray: [],
@@ -32,7 +37,7 @@ let asteroids = {
 let planets = {
   planetArray: [],
   numPlanets: 11,
-}
+};
 
 let planetWater = {
   x: 1100,
@@ -41,9 +46,9 @@ let planetWater = {
   fill: {
     r: 3,
     g: 150,
-    b: 250
-  }
-}
+    b: 250,
+  },
+};
 
 let planetFood = {
   x: 150,
@@ -52,24 +57,25 @@ let planetFood = {
   fill: {
     r: 75,
     g: 201,
-    b: 106
-  }
-}
+    b: 106,
+  },
+};
 
 let waterBar = {
   x: 20,
   y: 50,
   h: 20,
   l: 200,
-  speed: 0.35
-}
+  speed: 0.35,
+};
+
 let foodBar = {
   x: 20,
   y:100,
   h: 20,
   l: 200,
-  speed: 0.15
-}
+  speed: 0.15,
+};
 
 let titleString = "Start Game";
 let instructionString = 
@@ -84,7 +90,6 @@ let state = `title`;
 
 // set text settings, background, arrays
 function setup() {
-
     createCanvas(windowWidth, windowHeight);
     textSize(30);
     textAlign(CENTER, CENTER);
@@ -93,27 +98,25 @@ function setup() {
     for (let i = 0; i < asteroids.numAsteroids; i++) {
         let asteroid = new Asteroid();
         asteroids.asteroidArray.push(asteroid);
-      }
+    }
     
     for (let i = 0; i < planets.numPlanets; i++) {
         let planet = new Planet();
         planets.planetArray.push(planet);
-      }    
+    }    
 
     for (let i = 0; i < stars.numStars; i++) {
         let star = new Star();
         stars.starArray.push(star);
-      } 
+    } 
 }
 
 function draw() {
-
     background(4, 15, 51);
-
     for (let i = 0; i < stars.starArray.length; i++) {
       let star = stars.starArray[i];
       star.display();
-      }
+    }
     
     waterRefill();
     foodRefill();
@@ -123,22 +126,22 @@ function draw() {
       for (let i = 0; i < planets.planetArray.length; i++) {
         let planet = planets.planetArray[i];
         planet.display();
-        }
-        displayStartScreen();
       }
+        displayStartScreen();
+    }
       else if (state === `animation`) {
         useArrowKeys();
         for (let i = 0; i < planets.planetArray.length; i++) {
           let planet = planets.planetArray[i];
           planet.display();
-          }
+        }
         displayPlanets();
         for (let i = 0; i < asteroids.asteroidArray.length; i++) {
           let asteroid = asteroids.asteroidArray[i];
           asteroid.display(); 
           asteroid.move();
           asteroid.collision(astronaut);
-          }
+        }
         displayBars();
         displayAstronaut();
       }
@@ -186,14 +189,13 @@ function displayPlanets() {
   ellipse(planetWater.x, planetWater.y, planetWater.size);
   fill(188, 216, 245);
   text(waterString, planetWater.x, planetWater.y);
-
 // planet Food
   fill(planetFood.fill.r, planetFood.fill.g, planetFood.fill.b);
   ellipse(planetFood.x, planetFood.y, planetFood.size);
   fill(298, 240, 194);
   text(foodString, planetFood.x, planetFood.y);
 }
-
+// Game ends when food or water supply are too low
 function runOut() {
   if (waterBar.l < 1) {
     state = `ending`;
@@ -202,67 +204,67 @@ function runOut() {
     state = `ending`;
   }
 }
-
+// pressing the mouse starts the game and music
 function mousePressed() {
   if (state === `title`) {
-      state = `animation`;
-      spaceMusic.play();
-    }
+    state = `animation`;
+    spaceMusic.play();
   }
-
-  function displayBars() {
-    push();
-    noStroke();
-    rectMode(CORNER);
-  
-    fill(255);
-    rect(20, 50, 200, 20);
-    rect(20, 100, 200, 20);
-    
-    fill(3, 150, 250);
-    rect(waterBar.x, waterBar.y, waterBar.l, waterBar.h);
-    waterBar.l = waterBar.l - waterBar.speed;
-   
-    fill(18, 160, 8);
-    rect(foodBar.x, foodBar.y, foodBar.l, foodBar.h);
-    foodBar.l = foodBar.l - foodBar.speed;
-    pop();
+}
+// display supply bars
+function displayBars() {
+  push();
+  noStroke();
+  rectMode(CORNER);
+// Two white bars as background
+  fill(255);
+  rect(20, 50, 200, 20);
+  rect(20, 100, 200, 20);
+// blue bar that shows war supply and shortens over time
+  fill(3, 150, 250);
+  rect(waterBar.x, waterBar.y, waterBar.l, waterBar.h);
+  waterBar.l = waterBar.l - waterBar.speed;
+// green bar that shows food suppoly and shortens over time
+  fill(18, 160, 8);
+  rect(foodBar.x, foodBar.y, foodBar.l, foodBar.h);
+  foodBar.l = foodBar.l - foodBar.speed;
+  pop();
+}
+// When the astronaut hovers over the water planet, the water bar fills up
+function waterRefill() {
+  let d = dist(astronaut.x, astronaut.y, planetWater.x, planetWater.y);
+  if (d < planetWater.size/2 + astronaut.size/2) {
+    waterBar.l = 200;
   }
-
-  function waterRefill() {
-    let d = dist(astronaut.x, astronaut.y, planetWater.x, planetWater.y);
-    if(d < planetWater.size/2 + astronaut.size/2) {
-        waterBar.l = 200;
-    }
-  }
-  // Food bar returns to full when astronaut touches food planet
-  function foodRefill() {
+}
+// When the astroansut hovers over the food planet, the food bar fills up
+function foodRefill() {
   let d = dist(astronaut.x, astronaut.y, planetFood.x, planetFood.y);
-    if(d < planetFood.size/2 + astronaut.size/2) {
-        foodBar.l = 200;
-    }
+  if(d < planetFood.size/2 + astronaut.size/2) {
+    foodBar.l = 200;
   }
+}
 
-  function displayStartScreen(){
-    rectMode(CENTER);
-    push();
-    noStroke();
-    fill(4, 15, 51);
-    rect(width/2, height/4, 500, 150);
-    fill(181, 227, 247);
-    textSize(20);
-    text(instructionString, width / 2, height/ 4);
-    pop();
-    push();
-    strokeWeight(2);
-    fill(200, 50, 50);
-    rect(width/2, height/2, 200, 50);
-    pop();
-    push();
-    fill(255);
-    text(titleString, width / 2, height / 2);
-    pop();
-  }
+function displayStartScreen() {
+  rectMode(CENTER);
+  push();
+  noStroke();
+  fill(4, 15, 51);
+  rect(width/2, height/4, 500, 150);
+  fill(181, 227, 247);
+  textSize(20);
+  text(instructionString, width / 2, height/ 4);
+  pop();
+  push();
+  strokeWeight(2);
+  fill(200, 50, 50);
+  rect(width/2, height/2, 200, 50);
+  pop();
+  push();
+  fill(255);
+  text(titleString, width / 2, height / 2);
+  pop();
+}
 
   function displayEndingScreen(){
     push();
